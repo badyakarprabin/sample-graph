9426e3f259b7261ef6fcd70829658c6dd3ddaddb
prabin <prabin.drag01@gmail.com>
2018-01-20 14:17:58 +0545

    Add animation in chart

--- src/Gauge.js
+++ src/Gauge.js
@@ -1,166 +1,182 @@
 import React, { Component } from 'react';
 import { core as ZingChart } from 'zingchart-react';
 
+var myConfig = {
+    backgroundColor: 'none',
+    type: "gauge",
+    plot: {
+        arperture: 180,
+        csize: 4,
+        backgroundColor: 'none',
+        tooltip: {
+            visible: false
+        }
+    },
+    plotarea: {
+        backgroundColor: 'none',
+        borderWidth: 0,
+        margin: "100 0 0 0"
+    },
+    scaleR: {
+        minValue: -10.00,
+        maxValue: 10.00,
+        step: 5,
+        aperture: 180,
+        backgroundColor: 'none',
+        item: {
+            padding: 5,
+            fontColor: "#1E5D9E",
+            fontFamily: 'Montserrat',
+            offsetR: 0
+        },
+        tick: {
+            lineColor: '#D1D3D4',
+            placement: 'out',
+            lineColor: '#1E5D9E'
+        },
+        "minor-ticks": 5,
+        "minor-tick": {
+            "line-color": "white",
+            "visible": true,
+            "size": 7,
+            "placement": "inner"
+        },
+        center: {
+            size: 0,
+            borderColor: 'none',
+            backgroundColor: 'none'
+        },
+        ring: {
+            size: 3,
+            rules: [
+                {
+                    rule: '%v < -5',
+                    backgroundColor: '#00BAF2'
+                },
+                {
+                    rule: '%v >= -5 && %v < -0',
+                    backgroundColor: '#1E5D9E'
+                },
+                {
+                    rule: '%v >= 0 && %v < 5',
+                    backgroundColor: '#9B26AF'
+                },
+                {
+                    rule: '%v >= 5',
+                    backgroundColor: '#E80C60'
+                }
+            ]
+        }
+    },
+    series: [
+        {
+            "animation": {
+                "method": 5,
+                "effect": 2,
+                "speed": 2500
+            },
+            text: "Internal",
+            values: [10],
+            lineColor: "#00BAF2",
+            backgroundColor: "#1E5D9E",
+        },
+
+    ]
+};
+var myDataSimple = {
+    "type": "gauge",
+    backgroundColor: 'none',
+    "scale-r": {
+        "values": "-10:10:5",
+        "line-color": "none",
+        "guide": {
+            "background-color": "black", // Set the interior color of the gauge so that it is solid black, not alternating colors
+            "alpha": 1
+        },
+        "tick": {
+            "line-width": 3,
+            "line-color": "white",
+            "size": 14
+        },
+        "minor-ticks": 5,
+        "minor-tick": {
+            "line-color": "white",
+            "visible": true,
+            "size": 7,
+            "placement": "inner"
+        },
+        "item": {
+            "color": "white", // Set the color and font of the labels
+            "placement": "inner",
+            "offset-r": -55 // Move the labels to the inside
+        },
+        "aperture": 200,
+        "center": {
+            "background-color": "#CC0000",
+            "size": 11
+        },
+        "background-color": "black",
+        "ring": {
+            "type": "circle",
+            "size": 8,
+            "rules": [
+                {
+                    "rule": '%v >= -10 && %v < 0',
+                    "background-color": "red",
+                    "border-width": 0
+                },
+                {
+                    "rule": "%v>=0",
+                    "background-color": "green",
+                    "border-width": 0
+                }
+            ]
+        }
+    },
+    "labels": [
+        {
+            "text": "100%",
+            "color": "white",
+            "width": "6%",
+            "x": "47%",
+            "y": "76%"
+        }
+    ],
+    "series": [
+        {
+            "animation": {
+                "method": 5,
+                "effect": 2,
+                "speed": 2500
+            },
+            "values": [0],
+            "background-color": "limeGreen",
+            "shadow": false,
+            "size": 100, // Set the length of the needle
+            "csize": 10 // Set the width of the needle
+        }
+    ],
+    //real-time feed
+    refresh: {
+        type: 'feed',
+        transport: 'js',
+        url: 'feed()',
+        interval: 3000
+    },
+};
+
+window.feed = function (callback) {
+    var tick = {};
+    tick.plot0 = Math.ceil((Math.random() * 10));
+    callback(JSON.stringify(tick));
+};
+
 class Gauge extends Component {
     render() {
-        var myConfig = {
-            backgroundColor: 'none',
-            type: "gauge",
-            plot: {
-                arperture: 180,
-                csize: 4,
-                backgroundColor: 'none',
-                tooltip: {
-                    visible: false
-                }
-            },
-            plotarea: {
-                backgroundColor: 'none',
-                borderWidth: 0,
-                margin: "100 0 0 0"
-            },
-            scaleR: {
-                minValue: -10.00,
-                maxValue: 10.00,
-                step: 5,
-                aperture: 180,
-                backgroundColor: 'none',
-                item: {
-                    padding: 5,
-                    fontColor: "#1E5D9E",
-                    fontFamily: 'Montserrat',
-                    offsetR: 0
-                },
-                tick: {
-                    lineColor: '#D1D3D4',
-                    placement: 'out',
-                    lineColor: '#1E5D9E'
-                },
-                "minor-ticks": 5,
-                "minor-tick": {
-                    "line-color": "white",
-                    "visible": true,
-                    "size": 7,
-                    "placement": "inner"
-                },
-                center: {
-                    size: 0,
-                    borderColor: 'none',
-                    backgroundColor: 'none'
-                },
-                ring: {
-                    size: 3,
-                    rules: [
-                        {
-                            rule: '%v < -5',
-                            backgroundColor: '#00BAF2'
-                        },
-                        {
-                            rule: '%v >= -5 && %v < -0',
-                            backgroundColor: '#1E5D9E'
-                        },
-                        {
-                            rule: '%v >= 0 && %v < 5',
-                            backgroundColor: '#9B26AF'
-                        },
-                        {
-                            rule: '%v >= 5',
-                            backgroundColor: '#E80C60'
-                        }
-                    ]
-                }
-            },
-            series: [
-                {
-                    "animation": {
-                        "method": 5,
-                        "effect": 2,
-                        "speed": 2500
-                    },
-                    text: "Internal",
-                    values: [10],
-                    lineColor: "#00BAF2",
-                    backgroundColor: "#1E5D9E",
-                },
 
-            ]
-        };
-        var myDataSimple = {
-            "type": "gauge",
-            backgroundColor: 'none',
-            "scale-r": {
-                "values": "-10:10:5",
-                "line-color": "none",
-                "guide": {
-                    "background-color": "black", // Set the interior color of the gauge so that it is solid black, not alternating colors
-                    "alpha": 1
-                },
-                "tick": {
-                    "line-width": 3,
-                    "line-color": "white",
-                    "size": 14
-                },
-                "minor-ticks": 5,
-                "minor-tick": {
-                    "line-color": "white",
-                    "visible": true,
-                    "size": 7,
-                    "placement": "inner"
-                },
-                "item": {
-                    "color": "white", // Set the color and font of the labels
-                    "placement": "inner",
-                    "offset-r": -55 // Move the labels to the inside
-                },
-                "aperture": 200,
-                "center": {
-                    "background-color": "#CC0000",
-                    "size": 11
-                },
-                "background-color": "black",
-                "ring": {
-                    "type": "circle",
-                    "size": 8,
-                    "rules": [
-                        {
-                            "rule": "%v<80",
-                            "background-color": "red",
-                            "border-width": 0
-                        },
-                        {
-                            "rule": "%v>=80",
-                            "background-color": "limeGreen",
-                            "border-width": 0
-                        }
-                    ]
-                }
-            },
-            "labels": [
-                {
-                    "text": "100%",
-                    "color": "white",
-                    "width": "6%",
-                    "x": "47%",
-                    "y": "76%"
-                }
-            ],
-            "series": [
-                {
-                    "animation": {
-                        "method": 5,
-                        "effect": 2,
-                        "speed": 2500
-                    },
-                    "values": [10],
-                    "background-color": "limeGreen",
-                    "shadow": false,
-                    "size": 100, // Set the length of the needle
-                    "csize": 10 // Set the width of the needle
-                }
-            ]
-        };
         return (
             <div className="container">
+                <div className='header'>Analog Meter : Sample Meter </div>
                 <div className="rows">
                     <div className='col-lg-6'>
                         <ZingChart id="chart1" height="300" width="600" data={myDataSimple} />
--- src/TimeLine.js
+++ src/TimeLine.js
@@ -1,128 +1,218 @@
 import React, { Component } from 'react';
 import AmCharts from "@amcharts/amcharts3-react";
+import { core as ZingChart } from 'zingchart-react';
 
+
+var myConfig1 = {
+    //chart styling
+    type: 'line',
+    globals: {
+        fontFamily: 'Roboto',
+    },
+    backgroundColor: '#fff',
+    title: {
+        backgroundColor: '#1565C0',
+        text: 'Real-Time Line Chart',
+        color: '#fff',
+        height: '30x',
+    },
+    plotarea: {
+        marginTop: '80px'
+    },
+    crosshairX: {
+        lineWidth: 4,
+        lineStyle: 'dashed',
+        lineColor: '#424242',
+        marker: {
+            visible: true,
+            size: 9
+        },
+        plotLabel: {
+            backgroundColor: '#fff',
+            borderColor: '#e3e3e3',
+            borderRadius: 5,
+            padding: 15,
+            fontSize: 15,
+            shadow: true,
+            shadowAlpha: 0.2,
+            shadowBlur: 5,
+            shadowDistance: 4,
+        },
+        scaleLabel: {
+            backgroundColor: '#424242',
+            padding: 5
+        }
+    },
+    scaleY: {
+        guide: {
+            visible: false
+        },
+        values: '0:100:25'
+    },
+    tooltip: {
+        visible: true
+    },
+    //real-time feed
+    refresh: {
+        type: 'feed',
+        transport: 'js',
+        url: 'feed()',
+        interval: 500
+    },
+    plot: {
+        shadow: 1,
+        shadowColor: '#eee',
+        shadowDistance: '10px',
+        lineWidth: 5,
+        hoverState: { visible: false },
+        marker: { visible: false },
+        aspect: 'spline'
+    },
+    series: [{
+        values: [],
+        lineColor: '#2196F3',
+        text: 'Blue Line'
+    }, {
+        values: [],
+        lineColor: '#ff9800',
+        text: 'Orange Line'
+    }]
+};
+
+
+window.feed = function (callback) {
+    var tick = {};
+    tick.plot0 = parseInt(10 + 90 * Math.random(), 10);
+    callback(JSON.stringify(tick));
+};
 
 // Generate random data
 function generateData() {
-  var firstDate = new Date();
-  firstDate.setDate(firstDate.getDate() - 500);
-  firstDate.setHours(0, 0, 0, 0);
+    var firstDate = new Date();
+    firstDate.setDate(firstDate.getDate() - 500);
+    firstDate.setHours(0, 0, 0, 0);
 
-  var dataProvider = [];
+    var dataProvider = [];
 
-  for (var i = 0; i < 100; ++i) {
-    var newDate = new Date(firstDate);
-    var date = new Date(newDate.getDate() + i);
+    for (var i = 0; i < 100; ++i) {
+        var newDate = new Date(firstDate);
+        var date = new Date(newDate.getDate() + i);
 
-    date.setDate(i);
+        date.setDate(i);
 
-    dataProvider.push({
-      date: date,
-      value: Math.floor(Math.random() * 10)
-    });
-  }
+        dataProvider.push({
+            date: date,
+            value: Math.floor(Math.random() * 10)
+        });
+    }
 
-  return dataProvider;
+    return dataProvider;
 }
 
 
 // Component which contains the dynamic state for the chart
 class TimeLine extends Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      dataProvider: generateData(),
-      timer: null
-    };
-  }
+        this.state = {
+            dataProvider: generateData(),
+            timer: null
+        };
+    }
 
-  componentDidMount() {
-    this.setState({
-      // Update the chart dataProvider every 3 seconds
-      timer: setInterval(() => {
+    componentDidMount() {
         this.setState({
-          dataProvider: generateData()
+            // Update the chart dataProvider every 3 seconds
+            timer: setInterval(() => {
+                this.setState({
+                    dataProvider: generateData()
+                });
+            }, 1000)
         });
-      }, 1000)
-    });
-  }
+    }
 
-  componentWillUnmount() {
-    clearInterval(this.state.timer);
-  }
+    componentWillUnmount() {
+        clearInterval(this.state.timer);
+    }
 
-  render() {
-    const config = {
-      "type": "serial",
-      "theme": "dark",
-      "marginRight": 40,
-      "marginLeft": 40,
-      "autoMarginOffset": 20,
-      "mouseWheelZoomEnabled": true,
-      "graphs": [{
-        "id": "g1",
-        "balloon": {
-          "drop": true,
-          "adjustBorderColor": false,
-          "color": "#ffffff"
-        },
-        "bullet": "round",
-        "bulletBorderAlpha": 1,
-        "bulletColor": "#FFFFFF",
-        "bulletSize": 5,
-        "hideBulletsCount": 50,
-        "lineThickness": 2,
-        "title": "red line",
-        "useLineColorForBulletBorder": true,
-        "valueField": "value",
-        "balloonText": "<span style='font-size:18px;'>[[value]]</span>"
-      }],
-      "chartScrollbar": {
-        "graph": "g1",
-        "oppositeAxis": false,
-        "offset": 30,
-        "scrollbarHeight": 80,
-        "backgroundAlpha": 0,
-        "selectedBackgroundAlpha": 0.1,
-        "selectedBackgroundColor": "#888888",
-        "graphFillAlpha": 0,
-        "graphLineAlpha": 0.5,
-        "selectedGraphFillAlpha": 0,
-        "selectedGraphLineAlpha": 1,
-        "autoGridCount": true,
-        "color": "#AAAAAA"
-      },
-      "chartCursor": {
-        "pan": true,
-        "valueLineEnabled": true,
-        "valueLineBalloonEnabled": true,
-        "cursorAlpha": 1,
-        "cursorColor": "#258cbb",
-        "limitToGraph": "g1",
-        "valueLineAlpha": 0.2,
-        "valueZoomable": true
-      },
-      "valueScrollbar": {
-        "oppositeAxis": false,
-        "offset": 50,
-        "scrollbarHeight": 10
-      },
-      "categoryField": "date",
-      "categoryAxis": {
-        "parseDates": true,
-        "dashLength": 1,
-        "minorGridEnabled": true
-      },
-      "dataProvider": this.state.dataProvider
-    };
+    render() {
+        const config = {
+            "type": "serial",
+            "theme": "dark",
+            "marginRight": 40,
+            "marginLeft": 40,
+            "autoMarginOffset": 20,
+            "mouseWheelZoomEnabled": false,
+            "graphs": [{
+                "id": "g1",
+                "balloon": {
+                    "drop": true,
+                    "adjustBorderColor": false,
+                    "color": "#ffffff"
+                },
+                "bullet": "round",
+                "bulletBorderAlpha": 1,
+                "bulletColor": "#FFFFFF",
+                "bulletSize": 5,
+                "hideBulletsCount": 50,
+                "lineThickness": 2,
+                "title": "red line",
+                "useLineColorForBulletBorder": true,
+                "valueField": "value",
+                "balloonText": "<span style='font-size:18px;'>[[value]]</span>"
+            }],
+            "chartScrollbar": {
+                "graph": "g1",
+                "oppositeAxis": false,
+                "offset": 30,
+                "scrollbarHeight": 80,
+                "backgroundAlpha": 0,
+                "selectedBackgroundAlpha": 0.1,
+                "selectedBackgroundColor": "#888888",
+                "graphFillAlpha": 0,
+                "graphLineAlpha": 0.5,
+                "selectedGraphFillAlpha": 0,
+                "selectedGraphLineAlpha": 1,
+                "autoGridCount": true,
+                "color": "#AAAAAA"
+            },
+            "chartCursor": {
+                "pan": true,
+                "valueLineEnabled": true,
+                "valueLineBalloonEnabled": true,
+                "cursorAlpha": 1,
+                "cursorColor": "#258cbb",
+                "limitToGraph": "g1",
+                "valueLineAlpha": 0.2,
+                "valueZoomable": true
+            },
+            "valueScrollbar": {
+                "oppositeAxis": false,
+                "offset": 50,
+                "scrollbarHeight": 10
+            },
+            "categoryField": "date",
+            "categoryAxis": {
+                "parseDates": true,
+                "dashLength": 1,
+                "minorGridEnabled": true
+            },
+            "dataProvider": this.state.dataProvider
+        };
 
-    return (
-      <div className="TimeLine">
-        <AmCharts.React style={{ width: "100%", height: "500px" }} options={config} />
-      </div>
-    );
-  }
+        return (
+            <div className="TimeLine">
+                <div className='header'>Personal DaqView Plus : Sample Meter </div>
+                <div className="rows">
+                    <AmCharts.React style={{ width: "100%", height: "500px" }} options={config} />
+                    <div className='secondChart'>
+                        <ZingChart id="chart4" height="300" width="600" data={myConfig1} />
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default TimeLine;
