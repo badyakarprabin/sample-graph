fbd41ab9e9c3bbfbd42159ff39dc353ece93b0d9
prabin <prabin.drag01@gmail.com>
2018-01-22 23:23:00 +0545

    Update token

--- src/Opentok.js
+++ src/Opentok.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-
 import { OTSession, OTPublisher, OTStreams, OTSubscriber, createSession } from 'opentok-react';
 
 class Opentok extends React.Component {
@@ -9,11 +8,10 @@ class Opentok extends React.Component {
     }
 
     componentWillMount() {
-
         this.sessionHelper = createSession({
             apiKey: '46044372',
-            sessionId: '1_MX40NjA0NDM3Mn5-MTUxNjYzMzIzMTAwMH40Ry9BWUVHYWJrYVRhdkE0ZUtYV0cxTkt-fg',
-            token: 'T1==cGFydG5lcl9pZD00NjA0NDM3MiZzaWc9NzYyN2RkNDEyZjE4OTdiOWQ3ZTAyMzljMGZmMjExM2RhOTM2NDhmODpzZXNzaW9uX2lkPTFfTVg0ME5qQTBORE0zTW41LU1UVXhOall6TXpJek1UQXdNSDQwUnk5QldVVkhZV0pyWVZSaGRrRTBaVXRZVjBjeFRrdC1mZyZjcmVhdGVfdGltZT0xNTE2NjMzMzUxJm5vbmNlPTAuOTA3OTQ4NzI4NTE3OTY5NSZyb2xlPW1vZGVyYXRvciZleHBpcmVfdGltZT0xNTE2NjM2OTUwJmluaXRpYWxfbGF5b3V0X2NsYXNzX2xpc3Q9',
+            sessionId: '1_MX40NjA0NDM3Mn5-MTUxNjY0MjM0MDQ4N35sYkJwemRjV3duem9KQlRDUlVmM3BFQnZ-fg',
+            token: 'T1==cGFydG5lcl9pZD00NjA0NDM3MiZzaWc9MTZlNjA2ODhiNTQ4YjQ4NDY3N2E5Y2ViODc5MjE5NTg0NWUzNmYzNDpzZXNzaW9uX2lkPTFfTVg0ME5qQTBORE0zTW41LU1UVXhOalkwTWpNME1EUTROMzVzWWtKd2VtUmpWM2R1ZW05S1FsUkRVbFZtTTNCRlFuWi1mZyZjcmVhdGVfdGltZT0xNTE2NjQyMzU3Jm5vbmNlPTAuMzAzOTgwMzk1NDk4MzA2MjQmcm9sZT1wdWJsaXNoZXImZXhwaXJlX3RpbWU9MTUxNjY0NTk1NyZpbml0aWFsX2xheW91dF9jbGFzc19saXN0PQ==',
             onStreamsUpdated: streams => { this.setState({ streams }); }
         });
     }
@@ -29,16 +27,11 @@ class Opentok extends React.Component {
 
                 {this.state.streams.map(stream => {
                     return (
-                        <div id="videos">
-                            <div id="subscriber">
-                                <OTSubscriber
-                                    key={stream.id}
-                                    session={this.sessionHelper.session}
-                                    stream={stream}
-                                /></div>
-                            <div id="publisher"></div>
-                        </div>
-
+                        <OTSubscriber
+                            key={stream.id}
+                            session={this.sessionHelper.session}
+                            stream={stream}
+                        />
                     );
                 })}
             </div>
@@ -47,5 +40,3 @@ class Opentok extends React.Component {
 }
 
 export default Opentok;
-
-
